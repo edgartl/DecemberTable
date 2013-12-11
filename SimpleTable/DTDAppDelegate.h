a7ca7d0f93a8abc27b1ec0cb4c3152257dec95bd
Tom Edgar <edgartlMBPr@Toms-MacBook-Pro.local>
2013-12-11 11:38:03 -0600

    Initial Commit

---
+++ SimpleTable/DTDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTDAppDelegate.h
+//  SimpleTable
+//
+//  Created by Tom Edgar on 12/11/13.
+//  Copyright (c) 2013 Tom Edgar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
